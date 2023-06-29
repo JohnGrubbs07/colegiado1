bcdf510f355cc481236a6f78ac5d9e7a473db686
JohnGrubbs07 <133933629+JohnGrubbs07@users.noreply.github.com>
2023-06-28 18:52:13 -0600

    Add files via upload

---
+++ Spotify.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colegiado.pkg1;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class Spotify {
+    
+   int id;
+   String titulo;
+   String autor;
+   double duracion;
+   int fechaCreacion;
+   
+   public Spotify(int id, String titulo, String autor, double duraccion, int fechaCreacion){
+       this.id = id;
+       this.titulo = titulo;
+       this.autor = autor;
+       this.duracion = duracion;
+       this.fechaCreacion = fechaCreacion;
+   
+   }
+       
+           
+}
